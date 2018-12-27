ac71274c2cb9b2c839ff0ba3c71de7f71f2d3aaf
menep <menep@protonmail.com>
2018-12-27 16:23:27 +0100

    first popup version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles/main.scss";
+
+const imgs = document.querySelectorAll(".image");
+const popupCont = document.querySelector(".popup__container");
+
+imgs.forEach(img =>
+  img.addEventListener("click", e => {
+    popupCont.classList.remove("hidden");
+    popupCont.classList.add("visible");
+    const src = e.target.classList[1] + ".JPG";
+    console.log(src);
+    popupCont.style.backgroundImage = `url("./img/${src}")`;
+    popupCont.style.backgroundSize = "cover";
+    popupCont.style.backgroundPosition = "center";
+  })
+);
+
+window.addEventListener("keyup", e => {
+  if (e.key === "Escape") {
+    popupCont.classList.remove("visible");
+    popupCont.classList.add("hidden");
+  }
+});
