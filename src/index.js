58be7f501441555f223aeea4cd0a186b718d04c0
menep <menep@protonmail.com>
2018-12-28 19:30:11 +0100

    add function to buttons, refactor image structure

--- src/index.js
+++ src/index.js
@@ -1,23 +1,57 @@
 import "./styles/main.scss";
+import sel from "./scripts/selectors";
 
-const imgs = document.querySelectorAll(".image");
-const popupCont = document.querySelector(".popup__container");
+let counter;
+const imgCount = sel.imgs.length - 1;
 
-imgs.forEach(img =>
+// attach event listener to each img to open popup
+sel.imgs.forEach(img =>
   img.addEventListener("click", e => {
-    popupCont.classList.remove("hidden");
-    popupCont.classList.add("visible");
-    const src = e.target.classList[1] + ".JPG";
-    console.log(src);
-    popupCont.style.backgroundImage = `url("./img/${src}")`;
-    popupCont.style.backgroundSize = "cover";
-    popupCont.style.backgroundPosition = "center";
+    sel.popupCont.classList.remove("hidden");
+    sel.popupCont.classList.add("visible");
+    counter = e.target.dataset.num;
+    const src = `./img/image__${counter}.JPG`;
+    sel.popupImg.setAttribute("src", src);
   })
 );
 
+// click on prev button loads previous image in popup
+sel.prev.addEventListener("click", () => {
+  --counter;
+  if (counter < 0) counter = imgCount;
+  let counterStr;
+  if (counter >= 0 && counter <= 9) {
+    counterStr = `00${counter}`;
+  } else if (counter >= 10 && counter <= 99) {
+    counterStr = `0${counter}`;
+  }
+
+  const src = `./img/image__${counterStr}.JPG`;
+
+  sel.popupImg.setAttribute("src", src);
+});
+
+// click on prev button loads next image in popup
+sel.next.addEventListener("click", () => {
+  ++counter;
+  if (counter > imgCount) counter = 0;
+  let counterStr;
+  if (counter >= 0 && counter <= 9) {
+    counterStr = `00${counter}`;
+  } else if (counter >= 10 && counter <= 99) {
+    counterStr = `0${counter}`;
+  }
+
+  const src = `./img/image__${counterStr}.JPG`;
+
+  sel.popupImg.setAttribute("src", src);
+  console.log(src);
+});
+
 window.addEventListener("keyup", e => {
   if (e.key === "Escape") {
-    popupCont.classList.remove("visible");
-    popupCont.classList.add("hidden");
+    sel.popupCont.classList.remove("visible");
+    sel.popupCont.classList.add("hidden");
+    sel.popupImg.setAttribute("src", ""); // unset image
   }
 });
---
+++ src/scripts/selectors.js
@@ -0,0 +1,9 @@
+const sel = {
+  imgs: document.querySelectorAll(".image"),
+  popupCont: document.querySelector(".popup__container"),
+  popupImg: document.querySelector(".popup__image"),
+  prev: document.querySelector(".popup__prev"),
+  next: document.querySelector(".popup__next")
+};
+
+export default sel;
